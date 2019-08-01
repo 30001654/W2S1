eede257bada8608927e30d8ddfc072d55e13c369
Sefton Munro <30001654@student.toiohomai.ac.nz>
2019-08-01 16:36:34 +1200

    initial

--- W2S1/MainWindow.xaml.cs
+++ W2S1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,55 @@ namespace W2S1
         public MainWindow()
         {
             InitializeComponent();
+            ImageDictionary.createDictionary();
+            AddListItems.AddItemsToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(AddListItems.a());
+        }
+    }
+
+    public class ImageDictionary
+    {
+        public static Dictionary<string, string> imgDictionary = new Dictionary<string, string>();
+        public static void createDictionary()
+        {
+            imgDictionary.Add("1", "Cars");
+            imgDictionary.Add("2", "Cars");
+            imgDictionary.Add("3", "Lizards");
+            imgDictionary.Add("4", "Lizards");
+            imgDictionary.Add("5", "GCards");
+            imgDictionary.Add("6", "GCards");
+        }
+    }
+
+    public class AddListItems
+    {
+        public static ArrayList dictionaryList = new ArrayList(ImageDictionary.imgDictionary.Values);
+        public static ArrayList categoryList = new ArrayList();
+
+        public static void AddItemsToList()
+        {
+            categoryList.Add(dictionaryList[0]);
+            for (int i = 0; i < dictionaryList.Count; i++)
+            {
+                if (!categoryList.Contains(dictionaryList[i]))
+                {
+                    categoryList.Add(dictionaryList[i]);
+                }
+            }
+        }
+
+        public static string a()
+        {
+            string x = "";
+            for (int i = 0; i < categoryList.Count; i++)
+            {
+                x += categoryList[i];
+            }
+            return x;
         }
     }
 }
